10d0b4795018e766638627de10789961d15ef855
natty-light <75773163+natty-light@users.noreply.github.com>
2022-10-22 20:07:54 -0400

    gorm(structs) - updated fields in Medication struct - first attempt of defining associations

--- api/structs.go
+++ api/structs.go
@@ -1,26 +1,44 @@
 package api
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
+	gorm.Model
 	ID    uint
 	fname string
 	lname string
 }
 
 type Pet struct {
-	ID         uint
-	name       string
-	breed_ID   uint
-	species_ID uint
-	user_ID    uint
-	age        uint
-	image      uint
+	gorm.Model
+	ID          uint
+	user_ID     uint
+	user        User
+	name        string
+	breeds      []Breed
+	species     Species
+	age         uint
+	images      []Image
+	meals       []Meal
+	medications []Medication
+}
+
+type Species struct {
+	gorm.Model
+	ID            uint
+	name          string
+	binomial_name string
 }
 
 type Breed struct {
+	gorm.Model
 	ID          uint
 	species_ID  uint
+	species     Species
 	name        string
 	size        string
 	height_m    float64
@@ -36,13 +54,15 @@ type Breed struct {
 }
 
 type Meal struct {
+	gorm.Model
 	ID        uint
 	pet_ID    uint
 	frequency []time.Time
-	food      uint
+	food      []Food
 }
 
 type Food struct {
+	gorm.Model
 	ID          uint
 	category    string
 	targetAge   uint
@@ -50,19 +70,24 @@ type Food struct {
 }
 
 type Medication struct {
+	gorm.Model
 	ID        uint
 	pet_ID    uint
-	frequency []time.Time
-	medicine  uint
+	frequency uint
+	startDate time.Time
+	endDate   time.Time
+	medicine  Medicine
 }
 
 type Medicine struct {
+	gorm.Model
 	ID            uint
 	medication_ID uint
 	description   string
 }
 
 type HealthEvent struct {
+	gorm.Model
 	ID          uint
 	pet_ID      uint
 	name        string
@@ -71,6 +96,7 @@ type HealthEvent struct {
 }
 
 type Image struct {
+	gorm.Model
 	ID          uint
 	S3url       string
 	description string
