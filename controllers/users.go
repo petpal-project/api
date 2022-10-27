22ab648c85316609b06dc1ae093afc05535d51b9
natty-light <75773163+natty-light@users.noreply.github.com>
2022-10-27 18:04:13 -0400

    controller(user) first attempt of GetUser method - I think this is how contexts work

--- controllers/users.go
+++ controllers/users.go
@@ -4,12 +4,13 @@ import (
 	"pet-pal/api/models"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 func GetUserIdFromFirebaseId(tokenUID string, DB *gorm.DB) uint {
 	var user models.User
-	DB.Where("AccountID = ?", tokenUID).First(&user)
+	DB.Where("accountID = ?", tokenUID).First(&user)
 	return user.ID
 }
 
@@ -18,4 +19,16 @@ func PostUser(DB *gorm.DB, fname string, lname string, tokenUID string, userID u
 	model.ID, model.CreatedAt, model.UpdatedAt = userID, time.Now(), time.Now()
 	var user models.User = models.User{Model: model, AccountID: tokenUID, FirstName: fname, LastName: lname, Pets: nil}
 	DB.Create(&user)
+}
+
+func GetUser(DB *gorm.DB, c *gin.Context) *models.User {
+	userId, err := c.Get("user")
+	if !err {
+		var user models.User
+		DB.Where("id = ?", userId).First(&user)
+		return &user
+	} else {
+		c.JSON(400, gin.H{"error": "User does not exist"})
+		return nil
+	}
 }
