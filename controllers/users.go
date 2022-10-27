de6326411b9f0107bceccfd713a79556df59b95f
natty-light <75773163+natty-light@users.noreply.github.com>
2022-10-26 23:58:37 -0400

    middleware(users) first attempt at User Post fn. - function PostUser takes values for fields in User struct,   populates the object, and posts it to the DB.

--- controllers/users.go
+++ controllers/users.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"pet-pal/api/models"
+	"time"
 
 	"gorm.io/gorm"
 )
@@ -10,4 +11,11 @@ func GetUserIdFromFirebaseId(tokenUID string, DB *gorm.DB) uint {
 	var user models.User
 	DB.Where("AccountID = ?", tokenUID).First(&user)
 	return user.ID
+}
+
+func PostUser(DB *gorm.DB, fname string, lname string, tokenUID string, userID uint) {
+	var model gorm.Model = gorm.Model{}
+	model.ID, model.CreatedAt, model.UpdatedAt = userID, time.Now(), time.Now()
+	var user models.User = models.User{Model: model, AccountID: tokenUID, FirstName: fname, LastName: lname, Pets: nil}
+	DB.Create(&user)
 }
