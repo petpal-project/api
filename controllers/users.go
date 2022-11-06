8c96a91478ce5f2d817a1a65f170fcc901e5cba7
natty-light <75773163+natty-light@users.noreply.github.com>
2022-11-05 22:09:46 -0400

    feat(users) Added error handling to users

--- controllers/users.go
+++ controllers/users.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"pet-pal/api/config"
 	"pet-pal/api/models"
 
@@ -22,19 +23,29 @@ func PostUser(c *gin.Context) {
 	if err := c.BindJSON(&user); err != nil {
 		return
 	}
-	models.CreateUser(user, DB)
+	err := models.CreateUser(user, DB)
+	if err != nil {
+		c.JSON(500, "Internal Server Error")
+	} else {
+		c.JSON(200, fmt.Sprintf("User with ID %d Created", user.ID))
+	}
 }
 
 func GetUser(c *gin.Context) {
 	var DB *gorm.DB = config.DB
 	var user *models.User
+	var err error
 
 	userId, exists := c.Get("user")
 	if exists {
-		user = models.RetrieveUser(uint(userId.(int)), DB)
-		c.JSON(200, &user)
+		user, err = models.RetrieveUser(uint(userId.(int)), DB)
+		if err != nil {
+			c.JSON(500, "Internal Server Error")
+		} else {
+			c.JSON(200, &user)
+		}
 	} else {
-		c.JSON(400, ": (")
+		c.JSON(400, "Missing User ID in Authorization Header")
 	}
 }
 
@@ -43,8 +54,12 @@ func DeleteUser(c *gin.Context) {
 	userId, exists := c.Get("user")
 
 	if exists {
-		models.DeleteUser(uint(userId.(int)), DB)
-		c.JSON(200, gin.H{"success": "user deleted"})
+		err := models.DeleteUser(uint(userId.(int)), DB)
+		if err != nil {
+			c.JSON(500, "Internal Server Error")
+		} else {
+			c.JSON(200, gin.H{"success": "user deleted"})
+		}
 	} else {
 		c.JSON(400, gin.H{"error": "No User ID in request"})
 	}
