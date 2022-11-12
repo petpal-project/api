a94865636b7d1e06406f337591b8a10271f53cde
Joe <95554565+joerybarczyk@users.noreply.github.com>
2022-11-12 17:57:53 -0500

    fix(breed response): Response struct is bad idea - Just don't encode the species field in JSON it's that easy - get rekt

--- controllers/breeds.go
+++ controllers/breeds.go
@@ -11,31 +11,31 @@ import (
 
 func GetBreed(c *gin.Context) {
 	var DB *gorm.DB = config.DB
-	var breed *models.BreedResponse
 	bid, err := strconv.Atoi(c.Param("breedId"))
 
 	if err != nil {
 		c.JSON(400, "breedId must be numeric")
+		c.Abort()
+		return
 	} else {
-		breed, err = models.RetrieveBreed(uint(bid), DB)
+		breed, err := models.RetrieveBreed(uint(bid), DB)
 		if err != nil {
 			c.JSON(500, err.Error())
 		} else {
-			c.JSON(200, c.BindJSON(breed))
+			c.JSON(200, &breed)
 		}
 	}
 }
 
 func GetBreeds(c *gin.Context) {
 	var DB *gorm.DB = config.DB
-	var breeds *[]models.BreedResponse
 
 	sid, err := strconv.Atoi(c.Query("speciesId"))
 
 	if err != nil {
 		c.JSON(400, "speciesId must be numeric")
 	} else {
-		breeds, err = models.RetrieveBreeds(uint(sid), DB)
+		breeds, err := models.RetrieveBreeds(uint(sid), DB)
 		if err != nil {
 			c.JSON(500, err.Error())
 		} else {
