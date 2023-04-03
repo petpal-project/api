298fcac4b1ec20d0f3b830fad65d358112a7e118
Natasha <75773163+natty-light@users.noreply.github.com>
2023-04-03 17:50:12 -0400

    Refactor models/breeds.go

--- models/breeds.go
+++ models/breeds.go
@@ -4,28 +4,30 @@ import "gorm.io/gorm"
 
 type Breed struct {
 	gorm.Model
-	SpeciesID    uint `json:"speciesId"`
-	Species      Species`json:"-"`
-	Name         string `json:"name"`
-	Size         string `json:"size"`
-	HeightMale   string `json:"heightMale"`
-	HeightFemale string `json:"heightFemale"`
-	WeightMale   string `json:"weightMale"`
-	WeightFemale string `json:"weightFemale"`
-	Coat         string `json:"coat"`
-	CoatDesc     string `json:"coatDesc"`
-	Colors       string `json:"colors"`
-	ColorsDesc   string `json:"colorsDesc"`
-	Energy       string `json:"energy"`
-	Activities   string `json:"activities"`
+	SpeciesID    uint    `json:"speciesId"`
+	Species      Species `json:"-"`
+	Name         string  `json:"name"`
+	Size         string  `json:"size"`
+	HeightMale   string  `json:"heightMale"`
+	HeightFemale string  `json:"heightFemale"`
+	WeightMale   string  `json:"weightMale"`
+	WeightFemale string  `json:"weightFemale"`
+	Coat         string  `json:"coat"`
+	CoatDesc     string  `json:"coatDesc"`
+	Colors       string  `json:"colors"`
+	ColorsDesc   string  `json:"colorsDesc"`
+	Energy       string  `json:"energy"`
+	Activities   string  `json:"activities"`
 }
 
-func RetrieveBreeds(speciesId uint, DB *gorm.DB) (breeds *[]Breed, err error) {
-	err = DB.Model(&Breed{}).Find(&breeds, "species_id = ?", speciesId).Error
-	return
+func RetrieveBreeds(speciesId uint, DB *gorm.DB) (*[]Breed, error) {
+	var breeds *[]Breed
+	err := DB.Model(&Breed{}).Find(&breeds, "species_id = ?", speciesId).Error
+	return breeds, err
 }
 
-func RetrieveBreed(breedId uint, DB *gorm.DB) (breed *Breed, err error) {
-	err = DB.Model(&Breed{}).First(&breed, "id = ?", breedId).Error
-	return
+func RetrieveBreed(breedId uint, DB *gorm.DB) (*Breed, error) {
+	var breed *Breed
+	err := DB.Model(&Breed{}).First(&breed, "id = ?", breedId).Error
+	return breed, err
 }
