fd61a0533e27909f0c3d8ef43f56452282112058
Natasha <75773163+natty-light@users.noreply.github.com>
2023-04-03 17:48:31 -0400

    Refactor models/pets.go

--- models/pets.go
+++ models/pets.go
@@ -1,8 +1,6 @@
 package models
 
 import (
-	"errors"
-
 	"gorm.io/gorm"
 )
 
@@ -22,61 +20,30 @@ type Pet struct {
 
 func (pet Pet) GetUserID() uint { return pet.UserID }
 
-func (pet *Pet) BeforeUpdate(DB *gorm.DB) (err error) {
-	var petInDB *Pet
-	uid, userExists := DB.Get("user")
-	pid, petExists := DB.Get("pet")
-	if !userExists {
-		err = errors.New("missing user id")
-	} else if !petExists {
-		err = errors.New("missing pet id")
-	} else if err = DB.Select("user_id").First(&petInDB, "id = ?", pid).Error; err != nil {
-		return
-	} else if petInDB.UserID != uid {
-		err = errors.New("pet does not belong to user")
-	}
-	return
-}
+func (pet *Pet) BeforeUpdate(DB *gorm.DB) error { return checkOwnership[Pet]("pet", DB) }
 
-func (pet *Pet) BeforeDelete(DB *gorm.DB) (err error) {
-	var petInDB *Pet
-	uid, userExists := DB.Get("user")
-	pid, petExists := DB.Get("pet")
-	if !userExists {
-		err = errors.New("missing user id")
-	} else if !petExists {
-		err = errors.New("missing pet id")
-	} else if err = DB.Select("user_id").First(&petInDB, "id = ?", pid).Error; err != nil {
-		return
-	} else if petInDB.UserID != uid {
-		err = errors.New("pet does not belong to user")
-	}
-	return
-}
+func (pet *Pet) BeforeDelete(DB *gorm.DB) error { return checkOwnership[Pet]("pet", DB) }
 
-func CreatePet(pet *Pet, DB *gorm.DB) (err error) {
-	err = DB.Create(&pet).Error
-	return
-}
+func CreatePet(pet *Pet, DB *gorm.DB) error { return DB.Create(&pet).Error }
 
-func RetrievePet(petId uint, userId uint, DB *gorm.DB) (pet *Pet, err error) {
-	err = DB.First(&pet, "id = ? AND user_id = ?", petId, userId).Error
-	return
-}
-
-func RetrievePets(userId uint, DB *gorm.DB) (pets *[]Pet, err error) {
-	err = DB.Find(&pets, "user_id = ?", userId).Error
-	return
-}
-
-func UpdatePet(userId uint, petId uint, pet *Pet, DB *gorm.DB) (updatedPet *Pet, err error) {
-	err = DB.Set("user", userId).Set("pet", petId).Model(&pet).Where("id = ?", petId).Updates(&pet).Error
-	updatedPet = pet
-	return
-}
-
-func DeletePet(petId uint, userId uint, DB *gorm.DB) (err error) {
+func RetrievePet(petId uint, userId uint, DB *gorm.DB) (*Pet, error) {
 	var pet *Pet
-	err = DB.Set("user", userId).Set("pet", petId).Delete(&pet, "id = ? AND user_id = ?", petId, userId).Error
-	return
+	err := DB.First(&pet, "id = ? AND user_id = ?", petId, userId).Error
+	return pet, err
+}
+
+func RetrievePets(userId uint, DB *gorm.DB) (*[]Pet, error) {
+	var pets *[]Pet
+	err := DB.Find(&pets, "user_id = ?", userId).Error
+	return pets, err
+}
+
+func UpdatePet(userId uint, petId uint, pet *Pet, DB *gorm.DB) (*Pet, error) {
+	err := DB.Set("user", userId).Set("pet", petId).Model(&pet).Where("id = ?", petId).Updates(&pet).Error
+	return pet, err
+}
+
+func DeletePet(petId uint, userId uint, DB *gorm.DB) error {
+	var pet *Pet
+	return DB.Set("user", userId).Set("pet", petId).Delete(&pet, "id = ? AND user_id = ?", petId, userId).Error
 }
